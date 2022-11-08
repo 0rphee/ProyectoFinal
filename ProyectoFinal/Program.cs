7519742a344552ddd7270303901d40c6ceb9173e
0rphee <79347623+0rphee@users.noreply.github.com>
2022-11-07 21:33:17 -0600

    añadir menu principal y obtencion de datos

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -1,7 +1,24 @@
-using System;
+/*
+Se piden recetas de cocina, se ingresan características.
+Para los ingredientes de la receta podríamos crear otra estructura, struct Ingrediente, para poder 
+guardar el nombre y la cantidad del ingrediente en un solo array? Como ustedes lo vean mejor. También
+a saber si Vicky lo aceptaría. Por lo mientra solo deje un array para los nombres.
 
-namespace ProyectoFinal { 
-	class Program { 
+Para resultados se buscará por nombre, id.
+Se mostrará una tabla con cada receta y sus características
+
+
+*/
+
+namespace ProyectoFinal
+{
+    class Program { 
+		public struct Receta {
+			public int id;
+			public string nombre;
+			public string[] ingredientes; // TODO tal vez cambiarlo, ver inicio del archivo
+			public int porciones;
+		}
 		static void Main(string[] args) {
 			
 			// funciones genéricas que suelo utilizar
@@ -29,15 +46,60 @@ namespace ProyectoFinal {
 				for (int i = 0; i < numTitulos; i++) {
 					Console.WriteLine($"{i + 1}-{titulos[i]}");
 				}
-                Console.WriteLine($"{numTitulos+1}-SALIR");
+                Console.WriteLine($"{numTitulos+1}-SALIR\n");
 
-				return validacionInt(1, numTitulos+1, "Deme la opción deseada: ");
+				return validacionInt(1, numTitulos+1, "Deme la opción deseada:");
 			}
 			
-			Console.WriteLine("Hello World");
-			Console.ReadKey();
-			Console.WriteLine("");
-			Console.WriteLine("");
+			void entradaDatos(int numRecetas) {
+				for (int i = 0; i < numRecetas; i++) {
+
+					Console.WriteLine($"\nA continuación ingresará la información de la receta {i+1}");
+					
+					Receta currRecipe = new Receta();
+					currRecipe.id = i;
+					currRecipe.nombre = obtenerDato("Ingrese el nombre de la receta:");
+					currRecipe.porciones = validacionInt(0, 100, "Ingrese el número de porciones para las que la receta rinde:");
+					
+					int numIngredientes = validacionInt(0, 100, "Ingrese el número de ingredientes necesarios para realizar la receta:");
+					currRecipe.ingredientes = new string[numIngredientes];
+
+					for (int j = 0; j < numIngredientes; j++) {
+						currRecipe.ingredientes[j] = obtenerDato($"Ingrese el nombre del ingrediente {j+1}:");
+					}
+	
+				}
+				
+			}
+		
+			// INICIO PROPIO DEL PROGRAMA ------------------------------------------------------------------
+			
+			
+			// numero de recetas que se ingresarán al sistema
+			int nRecetas = validacionInt(0, 10, "¿Cuántas recetas vas a registar?");
+			Receta[] recetas = new Receta[nRecetas];
+			
+			int opc;
+			do 
+			{
+				opc = obtenerOpcMenu(new string[] {"Entrada de datos", "Uso de recetas"}, "MENU PRINCIPAL");
+				switch (opc)
+				{
+					case 1:
+						entradaDatos(nRecetas);
+						break;
+					case 2:
+						//usoDeRecetas();
+						break;
+					case 3:
+						// salida de menú, termina ejecución del programa
+						break;
+				}
+			} while (opc != 3);
+			
+			
+			
+			
 		}
 	} 
 }
