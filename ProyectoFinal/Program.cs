3d02e5110e394dd63dd38740b8a199982209f9d2
0rphee <79347623+0rphee@users.noreply.github.com>
2022-11-16 22:52:56 -0600

    change comments

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -2,15 +2,19 @@ namespace ProyectoFinal
 {
     class Program { 
 	    /* 
-	    The structure that will be applied through out all the program, in which we will be able to create the outputed table in which the input of the user will be showed. In this structure, some inputs will also be used for some Math equations, mostly for the outputed price. 
-	    The intended use for each class inside the structure is as follows:
-	    - 'id' will be used for the given item number, we find this in 'string' format for the unusable numbers that other classifications change or ignore. The maximmum lenght of characters (char) will be 5.
-	    - 'precio' shows the price of the clothing, its uses 'double' data type for the decimal use and for the discount.
-	    - 'nombre' will show the name of the clothing, the name uses 'string' for the usage of characters. Its important here that the name of the clothes needs to be inserted completely and correctly.
-	    - 'unidadesTalla' shows the Size of the clothing, in this class we will insert either of the following: 'chico'/'mediano'/'grande'. Once the user inputs one of the options showed before there will be a +1 made on the according size.
-	    - 'fechaEntrada' will be used to input the date in which the desired clothing is registered in the database, the format of the date will be dd / mm / yyyy.
-	    - 'fechaOferta' will be used for the registration of the time that the clothing has stayed in the database, the format will be the same as the one before.
+	    The structure that will be applied throughout all the program, in which we will be able to create the outputed table in which the
+		input of the user will be showed. In this structure, some inputs will also be used for some Math equations, mostly for the outputed 
+		price.The intended use for each property of the structure is detailed as follows:
+
+	    - 'id' will be used for the given item id number. The id is stored in 'string format for the unusable numbers that would be valid 
+		  for an id, but ignored by an 'int' type. The maximmum lenght of the string is 5.
+	    - 'precio' stores the price of the clothing item as a 'double' data type. Due to it being a price, it needs to use decimal values.
+	    - 'nombre' stores the name of the clothing item. Its important here that the name of the clothes needs to be inserted completely and correctly.
+	    - 'unidadesTalla' stores the units per size of the clothing item. It will store the items available for the next size classification 'chico'/'mediano'/'grande'.
+	    - 'fechaEntrada' stores the date in which the desired clothing is registered into the inventory, the format of the date will be dd / mm / yyyy.
+	    - 'fechaOferta' stores the date at which the item will be put on sale. It is calculated by adding 30 days to 'fechaEntrada'
 	    */
+
 		public struct Prenda {
 			public string id;
 			public double precio; // max 5 char
@@ -20,18 +24,16 @@ namespace ProyectoFinal
 			public DateTime fechaOferta; // dd/mm/yyyy
 		}
 	    
-	    	// The start of the Main program
+    	// The start of the Main program
 		static void Main(string[] args) {
 			
-
-			// Input that the user will type inside the console, the computer will place it as a string data type.
+			// Method that asks for user input to be typed in the console, returning it as a string value.
 			string obtenerDato(string textoAMostrar) {
 				Console.Write(textoAMostrar + " ");
 				return Console.ReadLine();
 			}
 
-			// This function is the validation that checks if all the variables with the data type 'int' are within the stablished maximum and mimimum limits (interval).
-			// The explanation used for this function works for all the other validations as well, int, string, and double validations.
+			// This function validates that an input of type 'int' is within an stablished maximum and mimimum limits (interval).
 			int validacionInt(int limInf, int limSup, string textoAMostrar) {
 				int dato;
 				do {
@@ -45,7 +47,7 @@ namespace ProyectoFinal
 				return dato; // Saves the input in the database
 			}
 
-			// This function is the validation that checks if all the variables with the data type 'double' are within the stablished maximum and mimimum limits (interval).
+			// This function validates that an input of type 'double' is within an stablished maximum and mimimum limits (interval).
 			double validacionDouble(double limInf, double limSup, string textoAMostrar) {
 				double dato;
 				do {
@@ -59,7 +61,7 @@ namespace ProyectoFinal
 				return dato;
 			}
 
-			// This function is the validation that checks if all the variables with the data type 'string' are within the stablished maximum and mimimum limits (interval).
+			// This function validates that the length of an input of type 'string' is within an stablished maximum and mimimum limits (interval).
 			string validacionLength(int limInf, int limSup, string textoAMostrar) {
 				string dato;
 				bool condicion;
@@ -75,7 +77,7 @@ namespace ProyectoFinal
 				return dato;
 			}
 
-			// Generates the corresponding Menu and returns an option of the menu.
+			// Generates a menu from an array of titles and returns the user-selected option for the menu.
 			int obtenerOpcMenu(string[] titulos, string mainTitle) {
 				int numTitulos = titulos.Length;
 
@@ -89,11 +91,12 @@ namespace ProyectoFinal
 				return validacionInt(1, numTitulos+1, "Deme la opción deseada:");
 			}
 			
-			// The array used for the second option in the menu ('case 2')
+			// Method that asks for the items to be stored. It's the second option in the main menu ('case 2')
 			void entradaDatos(Prenda[] prendas) {
-				for (int i = 0; i < prendas.Length; i++) { // Loops all the next commands according to the stablished size of the array that was asked at the beginning of the program to the user.
+				// Loops through all the next commands necessary to create a new item according to the established size of the array that was asked at the beginning of the program to the user (the number of items to be registered).
+				for (int i = 0; i < prendas.Length; i++) { 
 
-					Console.WriteLine($"\nA continuación ingresará la información de la prenda {i+1}"); // Reminds the user in which clothing he is.
+					Console.WriteLine($"\nA continuación ingresará la información de la prenda {i+1}"); // Reminds the user which clothing item they are registering.
 					
 					Prenda currPrenda = new Prenda();
 					currPrenda.id = validacionLength(5, 5, "Ingrese el ID de la prenda:"); // The input that will represent the ID. the function 'validacionLength' will place the validation in the input of the user, as we see: (5,5, ....) places the corresponding values to (limInf, limSup, .....), as such the ID needs to be equal to 5 characters.
@@ -108,20 +111,21 @@ namespace ProyectoFinal
 						currPrenda.unidadesTalla[k] = int.Parse(obtenerDato($"Ingrese el número de unidades de la talla {nombreTallas[k]}:")); // The input of the user that will determine the quantity of clothes in each size.
 					}
 
-					currPrenda.fechaEntrada = DateTime.Parse(obtenerDato("Dame la fecha de la prenda dd/mm/yyyy:"));
+					currPrenda.fechaEntrada = DateTime.Parse(obtenerDato("Deme la fecha de entrada de la prenda dd/mm/yyyy:"));
 					currPrenda.fechaOferta = currPrenda.fechaEntrada.Add(TimeSpan.FromDays(30)) ; 
 
 					prendas[i] = currPrenda;
 				}
 				ordenación(prendas);
 			}
-			
-			Prenda[] newArray(Prenda[] prendas, int LimInf, int LimSup) // los limInf yLimSup son los indices del array
+
+			// indiceInf and indiceSup are the indices limiting where the values of the new array will come from 			
+			Prenda[] newArray(Prenda[] prendas, int indiceInf, int    indiceSup)
 			{
-				Prenda[] NewArr = new Prenda[LimSup-LimInf];
+				Prenda[] NewArr = new Prenda[   indiceSup-indiceInf];
 				for (int k = 0; k < NewArr.Length; k++)
 				{
-					NewArr[k] = prendas[LimInf+k];
+					NewArr[k] = prendas[indiceInf+k];
 				}
 				return NewArr;
 			}
@@ -223,11 +227,11 @@ namespace ProyectoFinal
 							Console.WriteLine(busquedaNombre(prendas, nombreBuscar));
 							break;
 						case 4:
-							// Outputs, in table format, the 5 most expensive items.
+							// Outputs in table format, the 5 most expensive items.
 							Console.WriteLine(crearListaPrendas(newArray(prendas, 0, 4)));
 							break;
 						case 5:
-              // Outputs, in table format, the 5 most affordable clothes.
+					          // Outputs in table format, the 5 most affordable clothes.
 							Console.WriteLine(crearListaPrendas(newArray(prendas, prendas.Length-6, prendas.Length-1)));
 
 							break;
@@ -235,10 +239,10 @@ namespace ProyectoFinal
 							// Backs out to the first Menu
 							break;
 					}
-				} while (opc != 6 ); // If the choosen option is bigger than 6 it will repeat the menu. 
+				} while (opc != 6 ); // If the choosen option is bigger different to 6 it will repeat the menu. 
 			}
 			
-			// He next line of code searches the input of the user in the ID section.
+			// The next line of code searches the input of the user in the ID section.
 			string busquedaID(Prenda[] prendas, string idEncontrar){
 				for (int i = 0; i < prendas.Length; i++){ // It will loop the search for each clothing item inside the database.
 					if (prendas[i].id == idEncontrar){ 
@@ -248,7 +252,7 @@ namespace ProyectoFinal
 				return $"No se encontró ninguna prenda con el ID: {idEncontrar}"; // In the event that the if statement does not find anything and the loop has closed it will print the line of text.
 			}
 			
-			// He next line of code searches the input of the user in the Name section.
+			// The next line of code searches the input of the user in the Name section.
 			string busquedaNombre(Prenda[] prendas, string nombreEncontrar){
 			for (int i = 0; i < prendas.Length; i++){ // It will loop the search for each clothing item inside the database.
 					if (prendas[i].nombre == nombreEncontrar){
@@ -258,7 +262,7 @@ namespace ProyectoFinal
 				return $"No se encontró ninguna prenda con el nombre: {nombreEncontrar}"; // In the event that the if statement does not find anything and the loop has closed it will print the line of text.
 			}
 			
-			// This is te test unit that is used to quickly test the code, without the neccesity of placing the inputs ourselves each time we need to test something.
+			// This is the test unit that is used to quickly test the printing method, without the necessity of entering the inputs ourselves each time. 
 			void test(){
 				Prenda testprenda = new Prenda();
 				testprenda.id = "12345";
@@ -279,8 +283,6 @@ namespace ProyectoFinal
 			// Input of the number of clothes that the user will 
 			int nPrendas = validacionInt(1, 100, "¿Cuántas prendas vas a registar?"); // TODO definir numero minimo de prendas
 			Prenda[] prendas = new Prenda[nPrendas];
-			
-			
 			
 			int opc; // create the variable 'opc' for the menu, its usage is when choosing an option isnide of the menu.
 			bool datosRegistrados = false; // This statement is for the case that the user inputs the second option but hasn't registered the data.
